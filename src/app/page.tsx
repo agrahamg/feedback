8e59e167554c5abc046bf1628f3aab4e72bb2be3
Andrew Graham <andrew.grahamg@gmail.com>
2023-05-31 12:10:39 -0600

    move data fetching to api file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
-type Data = {
-  id: string;
-  name: string;
-  ticketCount: number;
-  colour: string;
-};
-
-const chartData = [
-  { id: "dep-1", name: "Legal", ticketCount: 32, colour: "#3F888F" },
-  { id: "dep-2", name: "Sales", ticketCount: 20, colour: "#FFA420" },
-  { id: "dep-3", name: "Engineering", ticketCount: 60, colour: "#287233" },
-  { id: "dep-4", name: "Manufacturing", ticketCount: 5, colour: "#4E5452" },
-  { id: "dep-5", name: "Maintenance", ticketCount: 14, colour: "#642424" },
-  {
-    id: "dep-6",
-    name: "Human Resourcing",
-    ticketCount: 35,
-    colour: "#1D1E33",
-  },
-  { id: "dep-7", name: "Events", ticketCount: 43, colour: "#E1CC4F" },
-] as const;
-
-const getData = () =>
-  new Promise((resolve) => {
-    setTimeout(resolve, 500, chartData);
-  });
+import {type DepartmentData, getData} from "@/api/chartData";
+import { BarChart } from "@/components/barChart";
 
 export default function Home() {
-  const [data, setData] = useState<Data[]>([]);
+  const [data, setData] = useState<DepartmentData[]>([]);
   const [sortOrder, setSortOrder] = useState("");
 
   const [isLoading, setIsLoading] = useState(true);
